0cda3832a1ccda7da759ba07d3a50e4ad3a69514
tamari-oz <72387668+tamari-oz@users.noreply.github.com>
2022-01-23 14:19:31 +0200

    Psd 122 (#1)
    * First commit of UA wrapper:
    1. Ability to get creds from IBM Secrets.
    2. Ability to get Organizations from Global org.
    
    * First commit of UA wrapper:
    1. Ability to get creds from IBM Secrets.
    2. Ability to get Organizations from Global org.
    3. Script to wrap WS Dockerfile with the tool modifications.
    
    * 1. Set CI
    2. Updated README.md
    3. Remove installing static ws-sdk.
    
    * Fixed Python Badge
    
    * Re-added flake package install
    
    * Enhanced README.md
    
    * 1. Added ability to choose to enable all pkg mans or none (-i).
    2. Updated documentation.
    3. Additional modifications to the code.
    
    * 1. Added ability to choose to enable all pkg mans or none (-i).
    2. Updated documentation.
    3. Additional modifications to the code.
    
    * 1. Now saving official docker file.
    2. CI will generate full image only when  tag is pushed.
    
    * Updated documentation

---
+++ ws_ua_wrapper/ua_wrapper.py
@@ -0,0 +1,63 @@
+import logging
+import os
+
+from ws_sdk import WSClient
+
+from _version import __tool_name__, __version__, __description__
+
+is_debug = logging.DEBUG if bool(os.environ.get("DEBUG", 0)) else logging.INFO
+
+logger = logging.getLogger(__tool_name__)
+logger.setLevel(is_debug)
+
+
+formatter = logging.Formatter('%(levelname)s %(asctime)s %(thread)d %(name)s: %(message)s')
+s_handler = logging.StreamHandler()
+s_handler.setFormatter(formatter)
+s_handler.setLevel(is_debug)
+logger.addHandler(s_handler)
+
+sdk_logger = logging.getLogger(WSClient.__module__)
+sdk_logger.setLevel(is_debug)
+sdk_logger.addHandler(s_handler)
+sdk_logger.propagate = False
+
+UA_DIR = "whitesource"
+URL = "saas"
+
+
+def get_ws_scopes() -> tuple:
+    project_name = os.environ['WS_PROJ_NAME']
+    product_token = os.environ['WS_PROD_TOKEN']
+    org_token = os.environ['WS_ORG_TOKEN']
+
+    return project_name, product_token, org_token
+
+
+def main():
+    logger.info(f"Starting {__description__}. Version {__version__}")
+    tool_details = (f"ps-{__tool_name__.replace('_','-')}", __version__)
+    user_key = os.environ['WS_USER_KEY']
+    project_name, product_token, org_token = get_ws_scopes()
+
+    ws_client = WSClient(user_key=user_key,
+                         token=org_token,
+                         url=URL,
+                         tool_details=tool_details,
+                         skip_ua_download=True,
+                         ua_path=os.path.join(os.getcwd(), UA_DIR))
+
+    scan_dir = os.environ.get('SCAN_DIR', "/SCAN_DIR")
+    logger.info(f"Scanning: '{scan_dir}' into WS Organization Token: '{org_token}', Product Token: '{product_token}', Project: '{project_name}'")
+
+    ret = ws_client.scan(scan_dir=scan_dir,
+                         product_token=product_token,
+                         project_name=project_name)
+
+    logger.info(f"Finished scanning: {scan_dir}")
+
+    return ret[0]
+
+
+if __name__ == '__main__':
+    main()
